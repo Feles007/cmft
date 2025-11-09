79a1b6b65e052078095c2e97f0215d3b1c741a65
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-11-08 20:24:05 -0800

    Add [[nodiscard]] to some Array methods

--- include/cmft/array.hpp
+++ include/cmft/array.hpp
@@ -15,10 +15,10 @@ public:
 	[[nodiscard]] T const& operator[](usize i) const {
 		return static_cast<Slice<const T>>(*this)[i];
 	}
-	operator Slice<T>() { // NOLINT(google-explicit-constructor)
+	[[nodiscard]] operator Slice<T>() { // NOLINT(google-explicit-constructor)
 		return Slice<T>(m_data, N);
 	}
-	operator Slice<const T>() const { // NOLINT(google-explicit-constructor)
+	[[nodiscard]] operator Slice<const T>() const { // NOLINT(google-explicit-constructor)
 		return Slice<const T>(m_data, N);
 	}
 
